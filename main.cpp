fbb8b53154d923af5ce9394cb0ebd2b706ccd35a
yumcyawiz <hashdolf1923@gmail.com>
2022-06-08 17:26:06 +0900

    init pipeline compile options

--- main.cpp
+++ main.cpp
@@ -36,12 +36,19 @@ class App
  public:
   App() {}
 
-  void init() { create_context(); }
+  void init()
+  {
+    create_context();
+    init_pipeline_compile_options();
+  }
   void render();
   void cleanup() { OPTIX_CHECK(optixDeviceContextDestroy(context)); }
 
  private:
   OptixDeviceContext context;
+
+  OptixPipelineCompileOptions pipeline_compile_options = {};
+  OptixModule module;
 
   void create_context()
   {
@@ -55,6 +62,26 @@ class App
     OPTIX_CHECK(optixDeviceContextCreate(cu_cxt, &options, &context));
   }
 
+  void init_pipeline_compile_options()
+  {
+    pipeline_compile_options.usesMotionBlur = false;
+    pipeline_compile_options.traversableGraphFlags =
+        OPTIX_TRAVERSABLE_GRAPH_FLAG_ALLOW_SINGLE_LEVEL_INSTANCING;
+    pipeline_compile_options.numPayloadValues = 2;
+    pipeline_compile_options.numAttributeValues = 2;
+    pipeline_compile_options.exceptionFlags = OPTIX_EXCEPTION_FLAG_NONE;
+    pipeline_compile_options.pipelineLaunchParamsVariableName = "params";
+  }
+
+  void create_module()
+  {
+    OptixModuleCompileOptions module_compile_options = {};
+    module_compile_options.maxRegisterCount =
+        OPTIX_COMPILE_DEFAULT_MAX_REGISTER_COUNT;
+    module_compile_options.optLevel = OPTIX_COMPILE_OPTIMIZATION_DEFAULT;
+    module_compile_options.debugLevel = OPTIX_COMPILE_DEBUG_LEVEL_MINIMAL;
+  }
+
   static void context_log_callback(unsigned int level, const char* tag,
                                    const char* message, void* cbdata)
   {
@@ -63,4 +90,12 @@ class App
   }
 };
 
-int main() { return 0; }
+int main()
+{
+  App app;
+  app.init();
+
+  app.cleanup();
+
+  return 0;
+}
