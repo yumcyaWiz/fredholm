c7e00f0a64a5db650b3ab8d2da404ca284e1f1f0
yumcyawiz <hashdolf1923@gmail.com>
2022-06-09 19:30:13 +0900

    separate util funcs to util.h

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+
 #include <cuda_runtime.h>
 #include <optix.h>
 #include <optix_function_table_definition.h>
@@ -13,43 +14,7 @@
 #include <stdexcept>
 #include <vector>
 
-#define CUDA_CHECK(call)                                                   \
-  do {                                                                     \
-    cudaError_t error = call;                                              \
-    if (error != cudaSuccess) {                                            \
-      std::stringstream ss;                                                \
-      ss << "CUDA call (" << #call << " ) failed with error: '"            \
-         << cudaGetErrorString(error) << "' (" __FILE__ << ":" << __LINE__ \
-         << ")\n";                                                         \
-      throw std::runtime_error(ss.str().c_str());                          \
-    }                                                                      \
-  } while (0)
-
-#define OPTIX_CHECK(call)                                                    \
-  do {                                                                       \
-    OptixResult res = call;                                                  \
-    if (res != OPTIX_SUCCESS) {                                              \
-      std::stringstream ss;                                                  \
-      ss << "Optix call '" << #call << "' failed: " __FILE__ ":" << __LINE__ \
-         << ")\n";                                                           \
-      throw std::runtime_error(ss.str().c_str());                            \
-    }                                                                        \
-  } while (0)
-
-#define OPTIX_CHECK_LOG(call)                                                \
-  do {                                                                       \
-    OptixResult res = call;                                                  \
-    const size_t sizeof_log_returned = sizeof_log;                           \
-    sizeof_log = sizeof(log); /* reset sizeof_log for future calls */        \
-    if (res != OPTIX_SUCCESS) {                                              \
-      std::stringstream ss;                                                  \
-      ss << "Optix call '" << #call << "' failed: " __FILE__ ":" << __LINE__ \
-         << ")\nLog:\n"                                                      \
-         << log << (sizeof_log_returned > sizeof(log) ? "<TRUNCATED>" : "")  \
-         << "\n";                                                            \
-      throw std::runtime_error(ss.str().c_str());                            \
-    }                                                                        \
-  } while (0)
+#include "util.h"
 
 std::vector<char> read_file(const std::filesystem::path& filepath)
 {
@@ -66,21 +31,6 @@ std::vector<char> read_file(const std::filesystem::path& filepath)
   file.close();
 
   return buffer;
-}
-
-template <typename T>
-CUdeviceptr alloc_and_copy_to_device(const T& src)
-{
-  // alloc memory on device
-  CUdeviceptr dst;
-  const size_t size = sizeof(T);
-  CUDA_CHECK(cudaMalloc(reinterpret_cast<void**>(&dst), size));
-
-  // copy data from host to device
-  CUDA_CHECK(cudaMemcpy(reinterpret_cast<void*>(dst), &src, size,
-                        cudaMemcpyHostToDevice));
-
-  return dst;
 }
 
 template <typename T>
@@ -115,7 +65,7 @@ class App
     create_shader_binding_table();
   }
 
-  void render();
+  void render() {}
 
   void cleanup()
   {
---
+++ src/util.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include <optix.h>
+
+#include <sstream>
+#include <stdexcept>
+
+#define CUDA_CHECK(call)                                                   \
+  do {                                                                     \
+    cudaError_t error = call;                                              \
+    if (error != cudaSuccess) {                                            \
+      std::stringstream ss;                                                \
+      ss << "CUDA call (" << #call << " ) failed with error: '"            \
+         << cudaGetErrorString(error) << "' (" __FILE__ << ":" << __LINE__ \
+         << ")\n";                                                         \
+      throw std::runtime_error(ss.str().c_str());                          \
+    }                                                                      \
+  } while (0)
+
+#define OPTIX_CHECK(call)                                                    \
+  do {                                                                       \
+    OptixResult res = call;                                                  \
+    if (res != OPTIX_SUCCESS) {                                              \
+      std::stringstream ss;                                                  \
+      ss << "Optix call '" << #call << "' failed: " __FILE__ ":" << __LINE__ \
+         << ")\n";                                                           \
+      throw std::runtime_error(ss.str().c_str());                            \
+    }                                                                        \
+  } while (0)
+
+#define OPTIX_CHECK_LOG(call)                                                \
+  do {                                                                       \
+    OptixResult res = call;                                                  \
+    const size_t sizeof_log_returned = sizeof_log;                           \
+    sizeof_log = sizeof(log); /* reset sizeof_log for future calls */        \
+    if (res != OPTIX_SUCCESS) {                                              \
+      std::stringstream ss;                                                  \
+      ss << "Optix call '" << #call << "' failed: " __FILE__ ":" << __LINE__ \
+         << ")\nLog:\n"                                                      \
+         << log << (sizeof_log_returned > sizeof(log) ? "<TRUNCATED>" : "")  \
+         << "\n";                                                            \
+      throw std::runtime_error(ss.str().c_str());                            \
+    }                                                                        \
+  } while (0)
+
+template <typename T>
+inline CUdeviceptr alloc_and_copy_to_device(const T& src)
+{
+  // alloc memory on device
+  CUdeviceptr dst;
+  const size_t size = sizeof(T);
+  CUDA_CHECK(cudaMalloc(reinterpret_cast<void**>(&dst), size));
+
+  // copy data from host to device
+  CUDA_CHECK(cudaMemcpy(reinterpret_cast<void*>(dst), &src, size,
+                        cudaMemcpyHostToDevice));
+
+  return dst;
+}
