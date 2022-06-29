4e28520caf00233b71cb43cacaf20b6429dd1237
yumcyawiz <hashdolf1923@gmail.com>
2022-06-29 20:11:34 +0900

    add trigonometric functions

---
+++ fredholm/modules/bxdf.cu
@@ -0,0 +1,88 @@
+__forceinline__ __device__ float cos_theta(const float3& w) { return w.y; }
+
+__forceinline__ __device__ float cos2_theta(const float3& w)
+{
+  return w.y * w.y;
+}
+
+__forceinline__ __device__ float abs_cos_theta(const float3& w)
+{
+  return fabs(w.y * w.y);
+}
+
+__forceinline__ __device__ float sin_theta(const float3& w)
+{
+  return sqrtf(fmax(1.0f - w.y * w.y, 0.0f));
+}
+
+__forceinline__ __device__ float sin2_theta(const float3& w)
+{
+  return fmax(1.0f - w.y * w.y, 0.0f);
+}
+
+__forceinline__ __device__ float abs_sin_theta(const float3& w)
+{
+  return fabs(sin_theta(w));
+}
+
+__forceinline__ __device__ float tan_theta(const float3& w)
+{
+  return sin_theta(w) / cos_theta(w);
+}
+
+__forceinline__ __device__ float tan2_theta(const float3& w)
+{
+  return 1.0f / (w.y * w.y) - 1.0f;
+}
+
+__forceinline__ __device__ float abs_tan_theta(const float3& w)
+{
+  return fabs(tan_theta(w));
+}
+
+__forceinline__ __device__ float sin_phi(const float3& w)
+{
+  return w.z / sqrtf(fmax(1.0f - w.y * w.y, 0.0f));
+}
+
+__forceinline__ __device__ float sin2_phi(const float3& w)
+{
+  return w.z * w.z / fmax(1.0f - w.y * w.y, 0.0f);
+}
+
+__forceinline__ __device__ float abs_sin_phi(const float3& w)
+{
+  return fabs(sin_phi(w));
+}
+
+__forceinline__ __device__ float cos_phi(const float3& w)
+{
+  return w.x / sqrtf(fmax(1.0f - w.y * w.y, 0.0f));
+}
+
+__forceinline__ __device__ float cos2_phi(const float3& w)
+{
+  return w.x * w.x / fmax(1.0f - w.y * w.y, 0.0f);
+}
+
+__forceinline__ __device__ float abs_cos_phi(const float3& w)
+{
+  return fabs(cos_phi(w));
+}
+
+struct MicrofacetReflection {
+  __device__ MicrofacetReflection() {}
+
+  __device__ float D(const float3& w)
+  {
+    const float t = w.x * w.x / (alpha.x * alpha.x) +
+                    w.z * w.z / (alpha.y * alpha.y) + w.y * w.y;
+    return 1.0f / (M_PI * alpha.x * alpha.y * t * t);
+  }
+
+  __device__ float G(const float3& w) {}
+
+  __device__ float lambda(const float3& w) {}
+
+  float2 alpha;
+};
