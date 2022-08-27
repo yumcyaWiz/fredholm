914cb4ff2fa01273ee76c069771439f4932b6e77
yumcyawiz <hashdolf1923@gmail.com>
2022-08-28 02:28:53 +0900

    impl uchimura tonemap

--- fredholm/kernels/include/kernels/post-process.h
+++ fredholm/kernels/include/kernels/post-process.h
@@ -25,6 +25,76 @@ static __forceinline__ __device__ float3 aces_tone_mapping(const float3& color)
                make_float3(0.0f), make_float3(1.0f));
 }
 
+static __forceinline__ __device__ float step(float edge, float x)
+{
+  return (x < edge) ? 0.0f : 1.0f;
+}
+
+static __forceinline__ __device__ float3 step(float edge, const float3& v)
+{
+  return make_float3(step(edge, v.x), step(edge, v.y), step(edge, v.z));
+}
+
+static __forceinline__ __device__ float smoothstep(float edge0, float edge1,
+                                                   float x)
+{
+  if (x < edge0)
+    return 0.0f;
+  else if (x > edge1)
+    return 1.0f;
+  else {
+    x = (x - edge0) / (edge1 - edge0);
+    return x * x * (3.0f - 2.0f * x);
+  }
+}
+
+static __forceinline__ __device__ float3 smoothstep(float edge0, float edge1,
+                                                    const float3& v)
+{
+  return make_float3(smoothstep(edge0, edge1, v.x),
+                     smoothstep(edge0, edge1, v.y),
+                     smoothstep(edge0, edge1, v.z));
+}
+
+// Uchimura 2017, "HDR theory and practice"
+// Math: https://www.desmos.com/calculator/gslcdxvipg
+// Source: https://www.slideshare.net/nikuque/hdr-theory-and-practicce-jp
+static __forceinline__ __device__ float3 uchimura(const float3& x, float P,
+                                                  float a, float m, float l,
+                                                  float c, float b)
+{
+  float l0 = ((P - m) * l) / a;
+  float L0 = m - m / a;
+  float L1 = m + (1.0 - m) / a;
+  float S0 = m + l0;
+  float S1 = m + a * l0;
+  float C2 = (a * P) / (P - S1);
+  float CP = -C2 / P;
+
+  float3 w0 = 1.0f - smoothstep(0.0, m, x);
+  float3 w2 = step(m + l0, x);
+  float3 w1 = 1.0f - w0 - w2;
+
+  float3 T =
+      m * make_float3(powf(x.x / m, c), powf(x.y / m, c), powf(x.z / m, c)) + b;
+  float3 S = P - (P - S1) * expf(CP * (x - S0));
+  float3 L = m + a * (x - m);
+
+  return T * w0 + L * w1 + S * w2;
+}
+
+static __forceinline__ __device__ float3 uchimura(const float3& x)
+{
+  const float P = 1.0;   // max display brightness
+  const float a = 1.0;   // contrast
+  const float m = 0.22;  // linear section start
+  const float l = 0.4;   // linear section length
+  const float c = 1.33;  // black
+  const float b = 0.0;   // pedestal
+
+  return uchimura(x, P, a, m, l, c, b);
+}
+
 // https://seblagarde.files.wordpress.com/2015/07/course_notes_moving_frostbite_to_pbr_v32.pdf
 static __forceinline__ __device__ float compute_EV100(float aperture,
                                                       float shutter_time,
--- fredholm/kernels/src/post-process.cu
+++ fredholm/kernels/src/post-process.cu
@@ -28,14 +28,16 @@ __global__ void post_process_kernel(const float4* beauty_in,
   const float EV100 = compute_EV100(1.0f, 1.0f, ISO);
   const float exposure = convert_EV100_to_exposure(EV100);
   color *= exposure;
-  color = aces_tone_mapping(color);
+  // color = aces_tone_mapping(color);
+  color = uchimura(color);
   color = linear_to_srgb(color);
   beauty_out[image_idx] = make_float4(color, 1.0f);
 
   // denoised
   color = make_float3(denoised_in[image_idx]);
   color *= exposure;
-  color = aces_tone_mapping(color);
+  // color = aces_tone_mapping(color);
+  color = uchimura(color);
   color = linear_to_srgb(color);
   denoised_out[image_idx] = make_float4(color, 1.0f);
 }
