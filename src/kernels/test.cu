2362816d77a4b2d010a382687210a5e77a010332
yumcyawiz <hashdolf1923@gmail.com>
2023-08-15 02:00:00 +0900

    render test image

--- src/kernels/test.cu
+++ src/kernels/test.cu
@@ -15,7 +15,8 @@ extern "C" __global__ void __raygen__rg()
     const uint3 dim = optixGetLaunchDimensions();
     const uint image_idx = idx.x + params.width * idx.y;
 
-    params.render_layer.beauty[image_idx] = make_float4(1.0f, 1.0f, 1.0f, 1.0f);
+    params.render_layer.beauty[image_idx] =
+        make_float4(idx.x / (float)dim.x, idx.y / (float)dim.y, 1.0f, 1.0f);
 }
 
 extern "C" __global__ void __miss__() {}
