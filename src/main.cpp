9547c42fb03a38515ae404eb2e0d45561ffc4e60
yumcyawiz <hashdolf1923@gmail.com>
2023-08-19 02:30:43 +0900

    refactor renderer

--- src/main.cpp
+++ src/main.cpp
@@ -8,33 +8,6 @@
 #include "render_strategy/simple/simple.h"
 #include "renderer.h"
 #include "scene.h"
-#include "stb_image_write.h"
-
-void save_png(const std::filesystem::path& filepath, const uint32_t width,
-              const uint32_t height, const float4* image)
-{
-    // convert float4 to uchar4
-    std::vector<uchar4> image_c4(width * height);
-    for (int j = 0; j < height; ++j)
-    {
-        for (int i = 0; i < width; ++i)
-        {
-            const int idx = i + width * j;
-            const float4 v = image[idx];
-            image_c4[idx].x = static_cast<unsigned char>(
-                std::clamp(255.0f * v.x, 0.0f, 255.0f));
-            image_c4[idx].y = static_cast<unsigned char>(
-                std::clamp(255.0f * v.y, 0.0f, 255.0f));
-            image_c4[idx].z = static_cast<unsigned char>(
-                std::clamp(255.0f * v.z, 0.0f, 255.0f));
-            image_c4[idx].w = 255;
-        }
-    }
-
-    // save image
-    stbi_write_png(filepath.c_str(), width, height, 4, image_c4.data(),
-                   sizeof(uchar4) * width);
-}
 
 int main()
 {
@@ -55,7 +28,9 @@ int main()
 
     {
         // init renderer
-        fredholm::Renderer renderer;
+        constexpr uint32_t width = 1920;
+        constexpr uint32_t height = 1080;
+        fredholm::Renderer renderer(width, height);
 
         fredholm::Camera camera(glm::vec3(0, 1, 2));
 
@@ -71,17 +46,9 @@ int main()
         renderer.set_render_strategy(&strategy);
 
         // render
-        constexpr uint32_t width = 1920;
-        constexpr uint32_t height = 1080;
-        fredholm::CUDABuffer<float4> beauty_d(width * height);
-        renderer.render(width, height, camera, scene_device,
-                        beauty_d.get_device_ptr());
+        renderer.render(camera, scene_device);
         renderer.synchronize();
-
-        // save image
-        std::vector<float4> beauty(width * height);
-        beauty_d.copy_d_to_h(beauty.data());
-        save_png("output.png", width, height, beauty.data());
+        renderer.save_image("output.png");
     }
 
     fredholm::optix_check(optixDeviceContextDestroy(context));
--- src/renderer.h
+++ src/renderer.h
@@ -7,18 +7,56 @@
 #include "render_strategy/render_strategy.h"
 #include "scene.h"
 #include "shared.h"
+#include "stb_image_write.h"
 #include "util.h"
 
 namespace fredholm
 {
 
+inline void write_image(const std::filesystem::path& filepath,
+                        const uint32_t width, const uint32_t height,
+                        const float4* image)
+{
+    // convert float4 to uchar4
+    std::vector<uchar4> image_c4(width * height);
+    for (int j = 0; j < height; ++j)
+    {
+        for (int i = 0; i < width; ++i)
+        {
+            const int idx = i + width * j;
+            const float4 v = image[idx];
+            image_c4[idx].x = static_cast<unsigned char>(
+                std::clamp(255.0f * v.x, 0.0f, 255.0f));
+            image_c4[idx].y = static_cast<unsigned char>(
+                std::clamp(255.0f * v.y, 0.0f, 255.0f));
+            image_c4[idx].z = static_cast<unsigned char>(
+                std::clamp(255.0f * v.z, 0.0f, 255.0f));
+            image_c4[idx].w = 255;
+        }
+    }
+
+    // save image
+    stbi_write_png(filepath.c_str(), width, height, 4, image_c4.data(),
+                   sizeof(uchar4) * width);
+}
+
 class Renderer
 {
    public:
-    Renderer() {}
+    Renderer(uint32_t width, uint32_t height) : width(width), height(height)
+    {
+        cuda_check(cuMemAlloc(&beauty, width * height * sizeof(float4)));
+        cuda_check(cuMemsetD32(beauty, 0, width * height));
+    }
 
     ~Renderer()
     {
+        if (beauty != 0)
+        {
+            cuda_check(cuMemFree(beauty));
+            beauty = 0;
+        }
+
         cuda_check(cuMemFree(sbt_record_set.raygen_records));
         cuda_check(cuMemFree(sbt_record_set.miss_records));
         cuda_check(cuMemFree(sbt_record_set.hitgroup_records));
@@ -33,11 +71,8 @@ class Renderer
         sbt = optix_create_sbt(sbt_record_set);
     }
 
-    // TODO: renderer should have width and height as a member variable?
-    // TODO: renderer should have AOVs as a member variable?
     // TODO: rendere should manage camera and scene?
-    void render(uint32_t width, uint32_t height, const Camera& camera,
-                const SceneDevice& scene, const CUdeviceptr& beauty)
+    void render(const Camera& camera, const SceneDevice& scene)
     {
         if (m_render_strategy)
         {
@@ -48,7 +83,20 @@ class Renderer
 
     void synchronize() const { cuda_check(cuCtxSynchronize()); }
 
+    void save_image(const std::filesystem::path& filepath) const
+    {
+        std::vector<float4> beauty_h(width * height);
+        cuda_check(cuMemcpyDtoH(beauty_h.data(), beauty,
+                                width * height * sizeof(float4)));
+        write_image(filepath, width, height, beauty_h.data());
+    }
+
    private:
+    uint32_t width = 0;
+    uint32_t height = 0;
+
+    CUdeviceptr beauty = 0;
+
     // TODO: this could be placed in render strategy?
     SbtRecordSet sbt_record_set;
     OptixShaderBindingTable sbt;
