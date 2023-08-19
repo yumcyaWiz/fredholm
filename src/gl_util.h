1f9feb3159f42b31584ae322a1d353160b634c60
yumcyawiz <hashdolf1923@gmail.com>
2023-08-19 15:38:37 +0900

    add gl quad

--- src/gl_util.h
+++ src/gl_util.h
@@ -403,6 +403,48 @@ class GLPipeline
     void deactivate() const { glBindProgramPipeline(0); }
 };
 
+class GLQuad
+{
+   private:
+    GLVertexArrayObject VAO;
+    GLBuffer vertex_buffer;
+    GLBuffer index_buffer;
+
+   public:
+    GLQuad()
+    {
+        // setup VBO
+        // position and texcoords
+        const std::vector<GLfloat> vertices = {
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f, 1.0f,  -1.0f, 0.0f, 1.0f, 0.0f,
+            1.0f,  1.0f,  0.0f, 1.0f, 1.0f, -1.0f, 1.0f,  0.0f, 0.0f, 1.0f};
+        vertex_buffer.setData(vertices.data(), vertices.size(), GL_STATIC_DRAW);
+
+        // setup EBO
+        const std::vector<GLuint> indices = {0, 1, 2, 2, 3, 0};
+        index_buffer.setData(indices.data(), indices.size(), GL_STATIC_DRAW);
+
+        // setup VAO
+        VAO.bindVertexBuffer(vertex_buffer, 0, 0, 5 * sizeof(GLfloat));
+        VAO.bindElementBuffer(index_buffer);
+
+        // position
+        VAO.activateVertexAttribution(0, 0, 3, GL_FLOAT, 0);
+
+        // texcoords
+        VAO.activateVertexAttribution(0, 1, 2, GL_FLOAT, 3 * sizeof(GLfloat));
+    }
+
+    void draw(const GLPipeline& pipeline) const
+    {
+        pipeline.activate();
+        VAO.activate();
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        VAO.deactivate();
+        pipeline.deactivate();
+    }
+};
+
 }  // namespace fredholm
 
 #endif
